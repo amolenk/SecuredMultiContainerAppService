b0cad00852e3d775b6b4632fa24623d08d693016
Sander Molenkamp <a.molenkamp@gmail.com>
2019-01-31 16:11:39 +0100

    Initial commit

---
+++ src/BackEnd/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly HttpClient _client = new HttpClient();
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            var requestUri = $"{Environment.GetEnvironmentVariable("EXTERNAL_SERVICE_URL")}/api/values";
+            var json = await _client.GetStringAsync(requestUri);
+            
+            return JsonConvert.DeserializeObject<string[]>(json);
+        }
+    }
+}
